92aae7f88f2aa1af602b335a2f26dc8887c01a3e
Maidele Archelus <maidele@usf.edu>
2025-02-16 14:27:29 -0500

    Task 2 - Employee Hourly Wage Calculation.

--- cc_7.js
+++ cc_7.js
@@ -10,3 +10,15 @@ function calculateInvoice(subtotal, taxRate, discount) {
 calculateInvoice(100, 0.08, 5); // Expected output: "Total Invoice: $103.00"
 calculateInvoice(500, 0.1, 20); // Expected output: "Total Invoice: $530.00"
 
+//Task 2-Employee Hourly Wage Calculation; Function Expression
+
+const calculateHourlyWage = function(salary, hoursPerWeek) {
+    let HourlyWage = salary / (hoursPerWeek * 52)
+    console.log(`Hourly Wage: $${HourlyWage.toFixed(2)}`);
+};
+
+// Test Data
+calculateHourlyWage(52000, 40); // Expected output: "Hourly Wage: $25.00"
+calculateHourlyWage(75000, 35); // Expected output: "Hourly Wage: $41.21"
+
+//Task 3-
